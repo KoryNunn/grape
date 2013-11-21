867277b1342e797c8be6d0cb73c975fe977e3e1d
Kory Nunn <knunn187@gmail.com>
2013-11-22 00:32:55 +1000

    improving tests

--- grape.js
+++ grape.js
@@ -4,6 +4,231 @@ var EventEmitter = require('events').EventEmitter,
 
 var nextTick = setImmediate || process.nextTick;
 
+function Test(name, testFunction){
+    this._plan = 0;
+    this._count = 0;
+    this._assersions = [];
+    this.name = name;
+    this._testFunction = testFunction;
+}
+
+// Unused currently.
+// Test.prototype = Object.create(EventEmitter.prototype);
+// Test.prototype.constructor = Test;
+
+Test.prototype.timeout = function(time){
+    timeout = Math.max(timeout, time);
+};
+
+Test.prototype.comment = function (message) {
+    // ToDo
+};
+
+Test.prototype.plan = function(ammount){
+    this._plan = ammount;
+};
+
+Test.prototype._run = function(){
+    var test = this;
+    try {
+        test._testFunction(this);
+    }
+    catch (err) {
+        test.error(err);
+    }
+};
+
+Test.prototype._assert = function(details){
+    this._count++;
+    if(this._ended){
+        if(details.operator === 'end' || details.operator === 'fail'){
+            return;
+        }
+        this.fail('asserted after test has ended');
+    }
+    this._assersions.push(details);
+};
+
+Test.prototype.end = function (message) {
+    var ok = this._plan === this._count;
+
+    if(this._ended){
+        return;
+    }
+
+    if(ok){
+        this._assert({
+            ok: true,
+            message: message,
+            operator: 'end'
+        });
+    }else{
+        this._assert({
+            ok: false,
+            message: 'plan != count',
+            operator: 'end'
+        });
+    }
+
+    this._ended = true;
+};
+
+Test.prototype.error = function(error, message){
+    this._assert({
+        ok: !error,
+        message : message || String(error),
+        operator : 'error',
+        actual : error
+    });
+};
+
+Test.prototype.pass = function(message){
+    this._assert({
+        ok: true,
+        message: message,
+        operator: 'pass'
+    });
+};
+
+Test.prototype.fail = function(message){
+    this._assert({
+        message: message,
+        operator: 'fail'
+    });
+};
+
+Test.prototype.skip = function(message){
+    this._assert({
+        message: message,
+        skip: true,
+        operator: 'skip'
+    });
+};
+
+Test.prototype.ok = function(value, message){
+    this._assert({
+        actual: value,
+        ok: !!value,
+        message: message,
+        operator: 'ok'
+    });
+};
+
+Test.prototype.notOk = function(value, message){
+    this._assert({
+        actual: value,
+        ok:!value,
+        message: message,
+        operator: 'notOk'
+    });
+};
+
+Test.prototype.equal = function(value, expected, message){
+    this._assert({
+        actual: value,
+        expected: expected,
+        ok: value === expected,
+        message: message,
+        operator: 'notOk'
+    });
+};
+
+Test.prototype.deepEqual = function(value, expected, message){
+    this._assert({
+        actual: value,
+        expected: expected,
+        ok: deepEqual(value, expected, { strict: true }),
+        message: message,
+        operator: 'deepEqual'
+    });
+};
+
+Test.prototype.deepLooseEqual = function(value, expected, message){
+    this._assert({
+        actual: value,
+        expected: expected,
+        ok: deepEqual(value, expected),
+        message: message,
+        operator: 'deepLooseEqual'
+    });
+};
+
+Test.prototype.notDeepEqual = function(value, expected, message){
+    this._assert({
+        actual: value,
+        expected: expected,
+        ok: !deepEqual(value, expected, { strict: true }),
+        message: message,
+        operator: 'notDeepEqual'
+    });
+};
+
+Test.prototype.notDeepLooseEqual = function(value, expected, message){
+    this._assert({
+        actual: value,
+        expected: expected,
+        ok: !deepEqual(value, expected),
+        message: message,
+        operator: 'notDeepLooseEqual'
+    });
+};
+
+Test.prototype['throws'] = function (fn, expected, message) {
+    var caughtError,
+        passed;
+
+    if(typeof expected === 'string'){
+        message = expected;
+        expected = undefined;
+    }
+
+    try{
+        fn();
+    }catch(error){
+        caughtError = {error: error};
+    }
+
+    passed = caughtError;
+
+    if(expected instanceof RegExp){
+        passed = expected.test(caughtError && caughtError.error);
+        expected = String(expected);
+    }
+
+    this._assert({
+        ok: passed,
+        message : message || 'should throw',
+        operator : 'throws',
+        actual : caughtError && caughtError.error,
+        expected : expected,
+        error: !passed && caughtError && caughtError.error
+    });
+};
+
+Test.prototype.doesNotThrow = function (fn, expected, message) {
+    var caughtError;
+
+    if(typeof expected === 'string'){
+        message = expected;
+        expected = undefined;
+    }
+
+    try{
+        fn();
+    }catch(error){
+        caughtError = { error : error };
+    }
+
+    this._assert({
+        ok: !caughtError,
+        message: message || 'should not throw',
+        operator: 'doesNotThrow',
+        actual: caughtError && caughtError.error,
+        expected: expected,
+        error: caughtError && caughtError.error
+    });
+};
+
 function instantiate(){
     var testsToRun = [],
         testsRun = [],
@@ -22,13 +247,17 @@ function instantiate(){
     }
 
     function complete(){
-        var results = [];
+        var results = encodeResults(testsRun);
 
         if(testsToRun.length !== totalTests){
             // tests level problem
         }
 
-        console.log(encodeResults(testsRun));
+        grape.emit('complete', results);
+
+        if(!grape.silent){
+            console.log(results);
+        }
     }
 
     function begin(){
@@ -36,7 +265,7 @@ function instantiate(){
             begun = true;
             nextTick(runNextTest);
             nextTick(function(){
-                if(typeof process === 'undefined'){
+                if(typeof process === 'undefined' || grape.useTimeout){
                     setTimeout(complete, timeout);
                 }else{
                     process.on('exit', complete);
@@ -50,214 +279,7 @@ function instantiate(){
         testsToRun.push(new Test(name, testFunction));
         begin();
     }
-
-    function Test(name, testFunction){
-        this._plan = 0;
-        this._count = 0;
-        this._assersions = [];
-        this.name = name;
-        this._testFunction = testFunction;
-    }
-    Test.prototype = Object.create(EventEmitter.prototype);
-    Test.prototype.constructor = Test;
-
-    Test.prototype.timeout = function(time){
-        timeout = Math.max(timeout, time);
-    };
-
-    Test.prototype.comment = function (message) {
-        this.emit('result', message.trim().replace(/^#\s*/, ''));
-    };
-
-    Test.prototype.plan = function(count){
-        this._plan = count;
-        this.emit('plan', count);
-    };
-
-    Test.prototype._run = function(){
-        var test = this;
-        try {
-            test._testFunction(this);
-        }
-        catch (err) {
-            test.error(err);
-        }
-    };
-
-    Test.prototype._assert = function(details){
-        if(details.operator === 'end'){
-            if(!details.ok){
-
-            }
-        }
-        this._count++;
-        this._assersions.push(details);
-    };
-
-    Test.prototype.end = function (message) {
-        var ok = this._plan === this._count;
-
-        if(this._ended){
-            return;
-        }
-
-        if(ok){
-            this._assert({
-                ok: true,
-                message: message,
-                operator: 'end'
-            });
-        }else{
-            this._assert({
-                ok: false,
-                message: 'plan != count',
-                operator: 'end'
-            });
-        }
-
-        this._ended = true;
-    };
-
-    Test.prototype.error = function(error, message){
-        this._assert({
-            ok: !error,
-            message : message || String(error),
-            operator : 'error',
-            actual : error
-        });
-    };
-
-    Test.prototype.pass = function(message){
-        this._assert({
-            ok: true,
-            message: message,
-            operator: 'pass'
-        });
-    };
-
-    Test.prototype.ok = function(value, message){
-        this._assert({
-            actual: value,
-            ok: !!value,
-            message: message,
-            operator: 'ok'
-        });
-    };
-
-    Test.prototype.notOk = function(value, message){
-        this._assert({
-            actual: value,
-            ok:!value,
-            message: message,
-            operator: 'notOk'
-        });
-    };
-
-    Test.prototype.equal = function(value, expected, message){
-        this._assert({
-            actual: value,
-            expected: expected,
-            ok:value === expected,
-            message: message,
-            operator: 'notOk'
-        });
-    };
-
-    Test.prototype.deepEqual = function(value, expected, message){
-        this._assert({
-            actual: value,
-            expected: expected,
-            ok: deepEqual(value, expected, { strict: true }),
-            message: message,
-            operator: 'deepEqual'
-        });
-    };
-
-    Test.prototype.deepLooseEqual = function(value, expected, message){
-        this._assert({
-            actual: value,
-            expected: expected,
-            ok: deepEqual(value, expected),
-            message: message,
-            operator: 'deepLooseEqual'
-        });
-    };
-
-    Test.prototype.notDeepEqual = function(value, expected, message){
-        this._assert({
-            actual: value,
-            expected: expected,
-            ok: !deepEqual(value, expected, { strict: true }),
-            message: message,
-            operator: 'notDeepEqual'
-        });
-    };
-
-    Test.prototype.notDeepLooseEqual = function(value, expected, message){
-        this._assert({
-            actual: value,
-            expected: expected,
-            ok: !deepEqual(value, expected),
-            message: message,
-            operator: 'notDeepLooseEqual'
-        });
-    };
-
-    Test.prototype['throws'] = function (fn, expected, message) {
-        var caughtError,
-            passed;
-
-        if(typeof expected === 'string'){
-            message = expected;
-            expected = undefined;
-        }
-
-        try{
-            fn();
-        }catch(error){
-            caughtError = {error: error};
-        }
-
-        passed = caughtError;
-
-        if(expected instanceof RegExp){
-            passed = expected.test(caughtError && caughtError.error);
-            expected = String(expected);
-        }
-
-        this._assert({
-            ok: passed,
-            message : message || 'should throw',
-            operator : 'throws',
-            actual : caughtError && caughtError.error,
-            expected : expected,
-            error: !passed && caughtError && caughtError.error
-        });
-    };
-
-    Test.prototype.doesNotThrow = function (fn, expected, message) {
-        var caughtError;
-
-        if(typeof expected === 'string'){
-            message = expected;
-            expected = undefined;
-        }
-
-        try{
-            fn();
-        }catch(error){
-            caughtError = { error : err };
-        }
-
-        this._assert({
-            ok: !caughtError,
-            message: message || 'should not throw',
-            operator: 'doesNotThrow',
-            actual: caughtError && caughtError.error,
-            expected: expected,
-            error: caughtError && caughtError.error
-        });
-    };
+    grape.__proto__ = Object.create(EventEmitter.prototype);
 
     grape.createNewInstance = instantiate;
 
--- test/all.js
+++ test/all.js
@@ -1,53 +1,79 @@
-var grape = require('../');
+var outerGrape = require('../');
 
-grape('doesn\'t explode', function(t){
-    t.plan(1);
+function createTestGrape(){
+    var grape = outerGrape.createNewInstance();
 
-    t.pass('passed');
+    grape.silent = true;
+    grape.useTimeout = true;
+
+    return grape;
+}
+
+outerGrape('grape pass', function(g){
+    var grape = createTestGrape();
+    g.plan(1);
+    g.doesNotThrow(function(){
+        grape('doesn\'t explode', function(t){
+            t.plan(1);
+
+            t.pass('passed');
+        });
+    }, "Grape shouldn't throw an error when called");
 });
 
-grape('t.ok', function(t){
 
-    t.plan(1);
+outerGrape('grape ok', function(g){
+    var grape = createTestGrape();
+    g.plan(1);
 
-    t.ok('ok', true);
+    grape.on('complete', function(){
+        g.pass();
+    });
+
+    grape('t.ok', function(t){
+
+        t.plan(1);
+
+        t.ok('ok', true);
+    });
 });
 
-grape('t.end ok', function(t){
+return;
+    grape('t.end ok', function(t){
 
-    t.plan(2);
+        t.plan(2);
 
-    t.ok(true, 'ok');
+        t.ok(true, 'ok');
 
-    t.ok(true, 'ok');
+        t.ok(true, 'ok');
 
-    t.end('ok');
-});
+        t.end('ok');
+    });
 
-grape('t.end not ok', function(t){
+    grape('t.end not ok', function(t){
 
-    t.plan(2);
+        t.plan(2);
 
-    t.ok(true, 'ok');
+        t.ok(true, 'ok');
 
-    t.end('ok');
+        t.end('ok');
 
-    t.ok(true, 'ok');
-});
+        t.ok(true, 'ok');
+    });
 
-grape('plan != count', function(t){
+    grape('plan != count', function(t){
 
-    t.plan(2);
+        t.plan(2);
 
-    t.ok(true, 'ok');
-});
+        t.ok(true, 'ok');
+    });
 
-grape('error', function(t){
+    grape('error', function(t){
 
-    t.plan(1);
+        t.plan(1);
 
-    // throw an error
-    a.b = c;
+        // throw an error
+        a.b = c;
 
-    t.ok(true, 'ok');
-});
+        t.ok(true, 'ok');
+    });
